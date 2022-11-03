ba17ec18b4b9aa16d54c829f415ace68adddbf35
unknown <marcan.grab@gmail.com>
2022-11-03 15:09:04 +0100

    added, uploading images through form, need to add diffrent models for gallery and download links

--- backend/freskbackend/views.py
+++ backend/freskbackend/views.py
@@ -15,4 +15,18 @@ class UserView(viewsets.ModelViewSet):
     queryset = User.objects.all()
 
 def add_wydarzenie(request):
-    return render(request, 'add_wydarzenie.html')
+    if request.method == "POST":
+
+        wydarzenie = Aktualnosci(
+            poster = request.user,
+            title = request.POST["tytul"],
+            title_image = request.FILES["title_image"],
+            content = request.POST["content"],
+            content_preview = request.POST["preview"],
+            archived = False,
+        )
+        wydarzenie.save()
+        return render(request, 'add_wydarzenie.html')
+
+    else:
+        return render(request, 'add_wydarzenie.html')
